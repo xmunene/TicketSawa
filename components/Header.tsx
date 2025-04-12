017e6eaad186e5dd7001d86be3a7f481095ed6e2
xmunene <vmunene42@gmail.com>
2025-04-12 14:14:49 +0300

    Header Added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,67 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/images/logo.png";
+import logo from "@/images/TicketSawa Logo.png";
+import { Sign } from "crypto";
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs";
+import SearchBar from "./SearchBar";
 
 function Header() {
   return (
       <div className="border-b">
-        <div>
+        <div className="flex flex-col lg:flex-row items-center gap-4 p-4">
+          <div className="flex items-center justify-between w-full lg:w-auto">
             <Link href="/" className="font-bold shrink-0">
                 <Image
                     src={logo}
                     alt="Logo"
-                    width={32}
-                    height={32}
-                    className="inline-block mr-2"
+                    width={100}
+                    height={100}
+                    className="w-10 lg:w-15"
                 />
-                </Link>  
+            </Link>
+            <div className="lg:hidden">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+              <SignedOut>
+                <SignInButton mode="modal">
+                  <button className="bg-blue-500 text-white px-3 py-1.5 text-sn rounded-lg hover:bg-blue-600 transition border border-gray-300">
+                  Sign In
+                  </button>
+                </SignInButton>
+              </SignedOut>
+            </div>  
+          </div>
+          <div className="w-full lg:max-w-2xl">
+            <SearchBar />
+          </div>
+
+          <div className="hidden lg:block ml-auto">
+            <SignedIn>
+              <div className="flex items-center gap-3">
+                <Link href="/seller"> 
+                  <button className="bg-blue-600 text-white px-3 py-1.5 text-sm rounded-lg hover:bg-blue-700 transition">
+                  Sell Tickets
+                  </button>
+                </Link> 
+
+                <Link href="/tickets"> 
+                  <button className="bg-gray-100 hover:text-gray-800 px-3 py-1.5 text-sm rounded-lg hover:bg-gray-200 transition border hover-gray-300">
+                  My Tickets
+                  </button>
+                </Link>
+                <UserButton /> 
+              </div>
+            </SignedIn>
+
+            <SignedOut>
+              <SignInButton mode="modal">
+                <button className="bg-blue-600 text-white px-3 py-1.5 text-sm rounded-lg hover:bg-blue-700 transition">
+                Sign In
+                </button>
+              </SignInButton>
+            </SignedOut>
+          </div>   
         </div>
       </div>
   )
---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { Search } from "lucide-react";
+import Form from 'next/form'
+
+function SearchBar() {
+  return (
+    <div>
+        <Form action={"/search"} className="relative">
+            <input
+            type="text"
+            name="q"
+            placeholder="Search for events, venues, or artists..."
+            className="w-full py-3 px-4 pl-12 bg-white rounded-xl border border-gray-200 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border transparent transition-all duration-200"
+            />
+            <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5"/>
+            <button 
+                type="submit" 
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-white-px-4 py-1.5 rounded-lg text-sm font-medium hover:bg-blue-300 transition-colors duration-200">
+                Search
+            </button>
+        </Form>
+    </div>
+  );
+}
+
+export default SearchBar
